f3fcc2438cd57a39d067a3bf34a2635824ac8bed
kmatiya <khormatiya@gmail.com>
2023-09-28 15:44:00 +0200

    Update config and main.py to call DbService

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
-from api.FolderCreator import FolderCreator
 from api.HttpReportService import HttpReportService
 from api.ReportGenerator import ReportGenerator
+from api.DbService import DbService
 from datetime import datetime
 # from config import config
 # if using pihmalawi
@@ -15,6 +14,11 @@ if __name__ == '__main__':
     print("Pulling reports from server has started")
     http_report_service.get_reports_from_server()
     report_generator = ReportGenerator(http_report_service.get_reports(), config)
-    report_generator.get_data_frame()
-
+    tracker_final_df, conditions_check_df = report_generator.get_data_frame()
+    for each_config in config["endpoints"]:
+        db_service = DbService(database=each_config["db_name"], user=each_config["db_user"], password=each_config['db_password'],
+                               host=each_config['db_host'],
+                               port=each_config["db_port"])
+        db_service.write_to_db("dhis2_report_summary", tracker_final_df)
+        db_service.write_to_db("data_element_validation", conditions_check_df)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- pihmalawi_config.py
+++ pihmalawi_config.py
@@ -1,8 +1,8 @@
 config = {
     "base_file_path": "./DHIS2 report",
     # tracker=DHIS2 Reporting Summary, full=DHIS2 Full Report
-    "report_generation": "tracker",
-    "tracker_file_name": "DHIS2 Reporting Summary",
+    "report_generation": "full",
+    "tracker_file_name": "dhis2_reporting_summary",
     "full_report_file_name": "DHIS2 Full Report",
     "data_elements_file_name": "data_elements.csv",
     "org_units_file_name": "org_units.csv",
@@ -15,8 +15,13 @@ config = {
             "report_file_name": "reports.csv",
             "use_start_date_in_request": True,
             "use_end_date_in_request": True,
-            "default_start_date": "2018-01-01",
-            "default_end_date": "2023-06-30",
+            "default_start_date": "2023-01-01",
+            "default_end_date": "2023-12-31",
+            "db_host": "",
+            "db_name": "",
+            "db_user": "",
+            "db_password": "",
+            "db_port": ""
         },
     ],
 }
