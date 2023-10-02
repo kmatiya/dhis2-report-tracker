00e287885c57111563498ecff00e8c3da6417632
kmatiya <khormatiya@gmail.com>
2023-10-02 14:57:10 +0200

    Remove for exception handling

--- main.py
+++ main.py
@@ -11,26 +11,22 @@ import schedule
 
 
 def generate_reports():
-    try:
-        print("Starting time" + str(datetime.now()))
-        http_report_service = HttpReportService(config)
-        print("Pulling reports from server has started")
-        http_report_service.get_reports_from_server()
-        print("Complete pulling reports from DHIS2 api")
-        report_generator = ReportGenerator(http_report_service.get_reports(), config)
-        tracker_final_df, conditions_check_df = report_generator.get_data_frame()
-        print("Complete generating reports")
-        for each_config in config["endpoints"]:
-            db_service = DbService(database=each_config["db_name"], user=each_config["db_user"],
-                                   password=each_config['db_password'],
-                                   host=each_config['db_host'],
-                                   port=each_config["db_port"])
-            db_service.write_to_db("dhis2_report_summary", tracker_final_df)
-            db_service.write_to_db("data_element_validation", conditions_check_df)
-        print("Ending saving in database at " + str(datetime.now()))
-    except Exception as e:
-        print("Process ran into an error")
-        print(e)
+    print("Starting time" + str(datetime.now()))
+    http_report_service = HttpReportService(config)
+    print("Pulling reports from server has started")
+    http_report_service.get_reports_from_server()
+    print("Complete pulling reports from DHIS2 api")
+    report_generator = ReportGenerator(http_report_service.get_reports(), config)
+    tracker_final_df, conditions_check_df = report_generator.get_data_frame()
+    print("Complete generating reports")
+    for each_config in config["endpoints"]:
+        db_service = DbService(database=each_config["db_name"], user=each_config["db_user"],
+                               password=each_config['db_password'],
+                               host=each_config['db_host'],
+                               port=each_config["db_port"])
+        db_service.write_to_db("dhis2_report_summary", tracker_final_df)
+        db_service.write_to_db("data_element_validation", conditions_check_df)
+    print("Ending saving in database at " + str(datetime.now()))
 
 
 # Press the green button in the gutter to run the script.
