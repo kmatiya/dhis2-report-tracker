3b8d9e7ef3649c61e054c13f20c8b7b46130ed08
kmatiya <khormatiya@gmail.com>
2023-10-04 01:41:51 +0200

    Update main with HttpReport service messages

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ import schedule
 
 
 def generate_reports():
+
     print("Starting time" + str(datetime.now()))
     http_report_service = HttpReportService(config)
     print("Pulling reports from server has started")
     http_report_service.get_reports_from_server()
     print("Complete pulling reports from DHIS2 api")
-    report_generator = ReportGenerator(http_report_service.get_reports(), config)
-    tracker_final_df, conditions_check_df = report_generator.get_data_frame()
-    print("Complete generating reports")
-    for each_config in config["endpoints"]:
-        db_service = DbService(database=each_config["db_name"], user=each_config["db_user"],
-                               password=each_config['db_password'],
-                               host=each_config['db_host'],
-                               port=each_config["db_port"])
-        db_service.write_to_db("dhis2_report_summary", tracker_final_df)
-        db_service.write_to_db("data_element_validation", conditions_check_df)
-    print("Ending saving in database at " + str(datetime.now()))
+    status_code, reports = http_report_service.get_reports()
+    if status_code == 200:
+        report_generator = ReportGenerator(reports, config)
+        tracker_final_df, conditions_check_df = report_generator.get_data_frame()
+        print("Complete generating reports")
+        for each_config in config["endpoints"]:
+            db_service = DbService(database=each_config["db_name"], user=each_config["db_user"],
+                                   password=each_config['db_password'],
+                                   host=each_config['db_host'],
+                                   port=each_config["db_port"])
+            db_service.write_to_db("dhis2_report_summary", tracker_final_df)
+            db_service.write_to_db("data_element_validation", conditions_check_df)
+        print("Ending saving in database at " + str(datetime.now()))
+    else:
+        print("There was an error getting reports from the server")
 
 
 # Press the green button in the gutter to run the script.
