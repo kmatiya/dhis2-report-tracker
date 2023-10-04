8b7bb55993d2b32122060f9b3f776382d379f043
kmatiya <khormatiya@gmail.com>
2023-10-04 02:01:27 +0200

    Update main report generator and db exception handling

--- main.py
+++ main.py
@@ -19,17 +19,20 @@ def generate_reports():
     print("Complete pulling reports from DHIS2 api")
     status_code, reports = http_report_service.get_reports()
     if status_code == 200:
-        report_generator = ReportGenerator(reports, config)
-        tracker_final_df, conditions_check_df = report_generator.get_data_frame()
-        print("Complete generating reports")
-        for each_config in config["endpoints"]:
-            db_service = DbService(database=each_config["db_name"], user=each_config["db_user"],
-                                   password=each_config['db_password'],
-                                   host=each_config['db_host'],
-                                   port=each_config["db_port"])
-            db_service.write_to_db("dhis2_report_summary", tracker_final_df)
-            db_service.write_to_db("data_element_validation", conditions_check_df)
-        print("Ending saving in database at " + str(datetime.now()))
+        try:
+            report_generator = ReportGenerator(reports, config)
+            tracker_final_df, conditions_check_df = report_generator.get_data_frame()
+            print("Complete generating reports")
+            for each_config in config["endpoints"]:
+                db_service = DbService(database=each_config["db_name"], user=each_config["db_user"],
+                                       password=each_config['db_password'],
+                                       host=each_config['db_host'],
+                                       port=each_config["db_port"])
+                db_service.write_to_db("dhis2_report_summary", tracker_final_df)
+                db_service.write_to_db("data_element_validation", conditions_check_df)
+            print("Ending saving in database at " + str(datetime.now()))
+        except Exception as e:
+            print(f"Error saving/accessing to db{e}")
     else:
         print("There was an error getting reports from the server")
 
@@ -37,7 +40,7 @@ def generate_reports():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     generate_reports()
-    '''print("starting scheduled tasks")
+    print("starting scheduled tasks")
     schedule.every(4).hours.do(generate_reports)
     # Loop so that the scheduling task
     # keeps on running all time.
@@ -45,6 +48,6 @@ if __name__ == '__main__':
         # Checks whether a scheduled task
         # is pending to run or not
         schedule.run_pending()
-        time.sleep(1)'''
+        time.sleep(1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
