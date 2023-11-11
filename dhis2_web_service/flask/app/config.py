a6e52d1206195d523b4f4cd631b92ad51c86a9b4
kmatiya <khormatiya@gmail.com>
2023-11-11 14:50:14 +0200

    Add config file for web service settings

---
+++ dhis2_web_service/flask/app/config.py
@@ -0,0 +1,13 @@
+config = {
+    "db_config": {
+        "db_host": "dhis2_postgres_container",
+        "db_name": "dhis2",
+        "db_user": "postgres",
+        "db_password": "9%4qP7b2H!%J",
+        "db_port": "5432"
+    },
+    "users": {
+        "user":"password",
+        "user1":"password"
+    }
+}
--- dhis2_web_service/flask/app/views.py
+++ dhis2_web_service/flask/app/views.py
@@ -1,22 +1,23 @@
 from app import app
 
-from flask import Flask
+from flask import Flask, abort
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-import pandas as pd
 
 from app.DbService import DbService
+from app.config import config
 
 auth = HTTPBasicAuth()
 
-users = {
-    "susan": generate_password_hash("bye")
-}
+users = config["users"]
+for each_user in users:
+    users[each_user] = generate_password_hash(users[each_user])
+db_config = config["db_config"]
 
-db_service = DbService(database="dhis2", user="postgres",
-                       password="",
-                       host="",
-                       port=5432)
+db_service = DbService(database=db_config["db_name"], user=db_config["db_user"],
+                       password=db_config["db_password"],
+                       host=db_config["db_host"],
+                       port=db_config["db_port"])
 
 data_elements_df = db_service.get_from_db("data_elements")
 category_option_combinations_df = db_service.get_from_db("category_option_combinations")
@@ -32,23 +33,27 @@ def verify_password(username, password):
 @app.route('/<table_name>')
 @auth.login_required
 def index(table_name):
-    table_df = db_service.get_from_db(table_name)
-    report_element_columns = table_df.columns.tolist()[9:]
-    for each_column_name in report_element_columns:
-        column_list = str(each_column_name).split("_")
-        if len(column_list) == 1:
-            data_element = data_elements_df[data_elements_df["id"] == column_list[0]]["name"].iat[0]
-            table_df.rename(columns={each_column_name: data_element}, inplace=True)
-        else:
-            data_element = data_elements_df[data_elements_df["id"] == column_list[0]]["name"].iat[0]
-            category_option_combination = category_option_combinations_df[category_option_combinations_df["id"] ==
-                                                                          column_list[1]]["name"].iat[0]
-            if category_option_combination is None:
-                table_df.rename(columns={each_column_name: data_element + " None"}, inplace=True)
+    try:
+        table_df = db_service.get_from_db(table_name)
+        table_df = table_df.replace([None], "")
+        report_element_columns = table_df.columns.tolist()[9:]
+        for each_column_name in report_element_columns:
+            column_list = str(each_column_name).split("_")
+            if len(column_list) == 1:
+                data_element = data_elements_df[data_elements_df["id"] == column_list[0]]["name"].iat[0]
+                table_df.rename(columns={each_column_name: data_element}, inplace=True)
             else:
-                table_df.rename(columns={each_column_name: f"{data_element} {category_option_combination}"},
-                                  inplace=True)
-    return table_df.to_html()
+                data_element = data_elements_df[data_elements_df["id"] == column_list[0]]["name"].iat[0]
+                category_option_combination = category_option_combinations_df[category_option_combinations_df["id"] ==
+                                                                              column_list[1]]["name"].iat[0]
+                if category_option_combination is None:
+                    table_df.rename(columns={each_column_name: data_element + " None"}, inplace=True)
+                else:
+                    table_df.rename(columns={each_column_name: f"{data_element} {category_option_combination}"},
+                                    inplace=True)
+        return table_df.to_html()
+    except:
+        abort(500)
 
 
 '''@app.route('/')
